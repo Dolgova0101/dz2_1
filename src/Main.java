74abd659a6d0a83f1465f073d0e2380f6c94c1cd
Katya <dolgova01011987@gmail.com>
2024-02-21 15:02:24 +0300

    Initiall commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13_676;
+        int bonus = amount / 20;
+
+        System.out.println("Всего миллей: " + bonus);
+
+    }
+}
